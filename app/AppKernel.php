c1efc7a969cba3a66c55a255c17651fc1281def0
David Weichert <info@davidweichert.de>
2015-02-24 22:07:50 +0100

    Remove default app bundle and move homepage to Gingy core bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Gingy\UserBundle\GingyUserBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
---
+++ src/Gingy/CoreBundle/Controller/HomepageController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of gingerbreadman-sightings.org.
+ *
+ * (c) David Weichert <info@davidweichert.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gingy\CoreBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomepageController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        $view = $request->getLocale() == 'de' ? 'GingyCoreBundle:Homepage:index.de.html.twig' : 'GingyCoreBundle:Homepage:index.en.html.twig';
+        return $this->render($view);
+    }
+}
